614072231d187f6a803087b1bf88fb7baa26adea
maziyue <871211390@qq.com>
2016-11-03 17:38:41 +0800

    Update FilterByTime.java

--- ex3/FilterByTime.java
+++ ex3/FilterByTime.java
@@ -1,3 +1,4 @@
+
 package No3;
 
 import java.io.FileInputStream;
@@ -5,7 +6,10 @@ import java.io.FileNotFoundException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * 题目要求：
@@ -18,21 +22,41 @@ import java.util.Scanner;
 public class FilterByTime {
 	
 	public static void main(String[] args) throws ParseException, FileNotFoundException{
+		
+		Locale locale = Locale.US; 
+		SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MMM/yyyy:HH:mm:ss", locale);
+
+		
 		SimpleDateFormat regularFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date beginDate = regularFormat.parse("2015-12-31 18:00:00");
 		Date endDate = regularFormat.parse("2015-12-31 19:00:00");
-		String filePath = "./access.log";
+		String filePath = "./access.log.txt";
 		FileInputStream inputStream = new FileInputStream(filePath);
 		Scanner scanner = new Scanner(inputStream, "UTF-8");
+		
+		String pattern = "(\\d+.\\d+.\\d+.\\d+) [^ ]* [^ ]* \\[([^ ]* [^ ]*)\\] \"[^ ]+ ([^ ]+) .*\" \\d+ \\d+ \"(.*)\" \"(.*)\"";
+		Pattern p = Pattern.compile(pattern);
 		while (scanner.hasNext()){
 			// 对每行进行处理
 			String line = scanner.nextLine();
 			// 切分获取IP，Time
-			String strIp = null;
-			String strTime = null;
+			String [] alt = line.split(" ");
+			String strIp = alt[0];
+			String strTime = alt[3].substring(1,alt[3].length()-1);
 			// 对在时间区间内的数据进行输出
 			System.out.println(strIp + "\t" + strTime);
+			
+			Matcher m = p.matcher(line);
+			 			if (m.find()) {
+			 				strIp = m.group(1);
+			 				strTime = m.group(2);
+			 				Date Time = inputFormat.parse(strTime);
+			 				if (Time.before(endDate) && Time.after(beginDate)) {
+			 					
+			 					// 对在时间区间内的数据进行输出
+			 					System.out.println(strIp + "\t" + regularFormat.format(Time));
+			 				}
+			 			}
 		}
 	}
-	
-}
+}	
